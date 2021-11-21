6777c7c1a92d4d4e2ea75dafc53fcb2806f676a0
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2021-11-21 13:53:36 +0100

    SimpleGUI implemented

--- src/it/unibo/oop/lab/mvc/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvc/SimpleGUI.java
@@ -1,9 +1,20 @@
 package it.unibo.oop.lab.mvc;
 
+import java.util.List;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -12,7 +23,7 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
-
+    private final Controller controller; 
     /*
      * Once the Controller is done, implement this class in such a way that:
      * 
@@ -36,9 +47,56 @@ public final class SimpleGUI {
 
     /**
      * builds a new {@link SimpleGUI}.
+     * @param controller
+     *                  the controller instance
      */
-    public SimpleGUI() {
-
+    public SimpleGUI(final Controller controller) {
+        this.controller = controller;
+        // Canvas
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        // Lower panel
+        final JPanel lower = new JPanel();
+        lower.setLayout(new BoxLayout(lower, BoxLayout.LINE_AXIS)); 
+        // Text field
+        final JTextField field = new JTextField();
+        field.setBackground(Color.lightGray);
+        // Text area
+        final JTextArea text = new JTextArea();
+        text.setEditable(false);
+        // Print button
+        final JButton print = new JButton("Print");
+        print.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                SimpleGUI.this.controller.setNextStringToPrint(field.getText());
+                SimpleGUI.this.controller.printCurrentString();
+            }
+        });
+        // Show history button
+        final JButton history = new JButton("Show History");
+        history.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                final StringBuilder sb = new StringBuilder();
+                final List<String> list = SimpleGUI.this.controller.getHistoryPrintedString();
+                for (final String print : list) {
+                    sb.append(print);
+                    sb.append('\n');
+                }
+                if (!list.isEmpty()) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+                text.setText(sb.toString());
+            }
+        });
+        // Assemble
+        lower.add(print);
+        lower.add(history);
+        canvas.add(lower, BorderLayout.SOUTH);
+        canvas.add(field, BorderLayout.NORTH);
+        canvas.add(text, BorderLayout.CENTER);
+        frame.setContentPane(canvas);
         /*
          * Make the frame half the resolution of the screen. This very method is
          * enough for a single screen setup. In case of multiple monitors, the
@@ -49,6 +107,7 @@ public final class SimpleGUI {
          * MUCH better than manually specify the size of a window in pixel: it
          * takes into account the current resolution.
          */
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
@@ -60,6 +119,19 @@ public final class SimpleGUI {
          * on screen. Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+    }
+
+    public void display() {
+        frame.setVisible(true);
+    }
+
+    /**
+     * 
+     * @param arg
+     *           ignored
+     */
+    public static void main(final String...arg) {
+        new SimpleGUI(new SimpleController()).display();
     }
 
 }
